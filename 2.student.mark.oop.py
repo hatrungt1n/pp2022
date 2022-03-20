e77b7bfb70b52ee207d203ff097f01842f3866e0
hatrungtin2002 <hatrungtin2k2@gmail.com>
2022-03-20 23:58:52 +0700

    Update

--- 2.student.mark.oop.py
+++ 2.student.mark.oop.py
@@ -3,15 +3,18 @@ class student:
         self.id = _id
         self.name = _name
         self.dob = _dob
+        # self.students.append(_id, _name, _dob)
 
 class course:
-    def __init__(self, _id, _name):
+    def __init__(self, _id, _name, _studentList, _marksList):
         self.id = _id
         self.name = _name
+        self.studentList = _studentList
+        self.marksList = _marksList
 
-class mark(student):
-    def __init__(self, _name):
-        self.name = _mark
+class mark():
+    def __init__(self, _mark):
+        self.mark = _mark
 
 students = []
 courses = []
@@ -38,10 +41,11 @@ for j in range(0, numOfCourses):
     print(f"Course {j+1}'s informations: ")
     id = input("Id: ")
     name = input("Name: ")
-    courses.append(course(id, name).__dict__)
+    studentsList = students
+    courses.append(course(id, name, studentsList).__dict__)
 
     j += 1
-
+print(courses)
 # Select a course, input marks for student in this course
 courseSelection = 1
 while courseSelection in range(1, numOfCourses + 1):
@@ -60,11 +64,8 @@ while courseSelection in range(1, numOfCourses + 1):
         marks.append(mark(_mark).__dict__)
 
         i += 1
-
-    # courses[courseSelection - 1]["marks"] = marks[
-    #     (courseSelection - 1) * numOfStudents : courseSelection * numOfStudents
-    # ]
+    courses.append(course(marks).__dict__)
 
 print(courses)
-print(students)
-print(marks)
+# print(students)
+# print(marks)
